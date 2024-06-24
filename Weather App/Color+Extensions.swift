a976378e91eda40fadea60ce59333ca8a36a7ac9
Christopher Alves <christopher.alves.dev@gmail.com>
2024-06-24 20:43:17 -0300

    feat: Create Weather App UI

---
+++ Weather App/Color+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Color+Extensions.swift
+//  Weather App
+//
+//  Created by Christopher Alves on 22/06/24.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let contrastColor = UIColor(named: "contrastColor")
+    static let mainColor = UIColor(named: "mainColor")
+}
